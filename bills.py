fedb60c9f5cfb911be6c9571cea9d114a980e271
morristheaverage <morristhomas26@gmail.com>
2020-09-16 16:41:49 +0100

    Fixed an extra slash in urls

--- bills.py
+++ bills.py
@@ -2,28 +2,61 @@
 currently before parliament
 """
 
+import codecs
+from datetime import date
 from bs4 import BeautifulSoup
 import requests
+
 
 
 def cache_request(url: str) -> str:
     """Handles requests for web pages and saves them
     locally, enabling the script to be run repeatedly offline
+
+    Args:
+        url (str): A url to be requested
+
+    Returns:
+        str: The text of the response to the request e.g. a html file
     """
     cache = "html-cache/"
     url_hash = str(hash(url))
+    enc = 'utf-8'
     try:
-        with open(cache + url_hash, 'r') as fp:
-            html = fp.read()
+        with codecs.open(cache + url_hash, mode='r', encoding=enc) as file:
+            html = file.read()
     except FileNotFoundError:
         html = requests.get(url).text
-        with open(cache + url_hash, 'w') as fp:
-            fp.write(html)
+        with codecs.open(cache + url_hash, mode='w', encoding=enc) as file:
+            file.write(html)
     return html
 
+def dop(datestr: str) -> int:
+    """Takes a date and returns the nth day of the
+    current parliament on which the date falls
+
+    Args:
+        datestr (str): A date in DD/MM/YYYY format
+
+    Returns:
+        int: The value n, where datestr is n days after the last election
+    """
+    # Reference value
+    start = date(2019, 12, 12) # Election was 12th Dec 2019
+
+    # Create new date object
+    day, month, year = (int(val) for val in datestr.split('.'))
+    day = date(year, month, day)
+
+    # Return difference
+    return (day - start).days
+
+
+
+
 # Generate the total soup
-URL = "https://services.parliament.uk/"
-HTML = cache_request(URL + "Bills/")
+URL = "https://services.parliament.uk"
+HTML = cache_request(URL + "/Bills/")
 soup = BeautifulSoup(HTML, 'html5lib')
 
 # Find all bills and categorise them accordingly
@@ -49,5 +82,14 @@ print(f"{len(ras)} bills granted Royal Assent.")
 commons_stages = {}
 for bill in commons:
     data = bill('td', 'bill-item-description')[0].a
-    commons_stages[data.text.strip()] = data['href'][:-5] + 'stages.html'
+    commons_stages[data.text.strip()] = data['href'][:-5] + '/stages.html'
     print(f"{data.text.strip():100}{data['href']}")
+
+print("Page titles")
+# Load up stages.html pages into cache
+for url_end in commons_stages.values():
+    page = URL + url_end
+    html_stage = cache_request(page)
+    print(page)
+    soup = BeautifulSoup(html_stage, "html5lib")
+    print(soup.title.text.strip())
